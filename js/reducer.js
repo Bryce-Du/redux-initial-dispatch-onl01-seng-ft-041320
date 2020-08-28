f108123f6291992ac2d5f0ffa98fcfee47c306b3
Bryce-Du <bryce.dumas@gmail.com>
2020-08-28 15:40:46 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -4,7 +4,7 @@ function changeState(state, action){
     switch (action.type) {
       case 'INCREASE_COUNT':
         return {count: state.count + 1}
-      default:
+      default: 
         return state;
     }
   }
